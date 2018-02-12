1224112e8ef742395e92f491c729582f6ac424c0
Birat Rai <biratrai@hotmail.com>
2018-02-11 23:07:59 -0500

    adding a movieDao method for room Dao

---
+++ app/src/main/java/com/gooner10/popularmoviesapp/Activity/data/MovieDao.java
@@ -0,0 +1,24 @@
+package com.gooner10.popularmoviesapp.Activity.data;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+/**
+ * MovieDao for Room
+ */
+
+@Dao
+public interface MovieDao {
+    @Query("SELECT * FROM MovieData")
+    List<MovieData> getAllFavoritMovie();
+
+    @Insert
+    void insertAll(MovieData... movieData);
+
+    @Delete
+    void delete(MovieData movieData);
+}
