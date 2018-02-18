43a1aa436ba6f31af43a90e465f1db08e43efbab
Birat Rai <biratrai@hotmail.com>
2018-02-18 11:10:46 -0500

    using the correct builder

--- app/src/main/java/com/gooner10/popularmoviesapp/Activity/data/MovieDatabase.java
+++ app/src/main/java/com/gooner10/popularmoviesapp/Activity/data/MovieDatabase.java
@@ -10,12 +10,15 @@ import android.content.Context;
  */
 @Database(entities = {MovieData.class}, version = 1)
 public abstract class MovieDatabase extends RoomDatabase {
+    private static final String MOVIE_DATABASE_NAME = "movie.db";
     private static MovieDatabase INSTANCE;
 
     public static MovieDatabase getInMemoryDatabase(Context context) {
         if (INSTANCE == null) {
             INSTANCE =
-                    Room.inMemoryDatabaseBuilder(context.getApplicationContext(), MovieDatabase.class)
+                    Room.databaseBuilder(context.getApplicationContext(),
+                            MovieDatabase.class,
+                            MOVIE_DATABASE_NAME)
                             // To simplify the codelab, allow queries on the main thread.
                             // Don't do this on a real app! See PersistenceBasicSample for an example.
                             .allowMainThreadQueries()
