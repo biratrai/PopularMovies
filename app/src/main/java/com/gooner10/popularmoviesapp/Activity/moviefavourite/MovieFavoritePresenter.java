e4c497f57d1794a46586601b5b307f5e67ba5100
Birat Rai <biratrai@hotmail.com>
2018-02-18 18:02:28 -0500

    adding favorite movie presenter

---
+++ app/src/main/java/com/gooner10/popularmoviesapp/Activity/moviefavourite/MovieFavoritePresenter.java
@@ -0,0 +1,12 @@
+package com.gooner10.popularmoviesapp.Activity.moviefavourite;
+
+/**
+ * Presenter for the FavoriteFragment
+ */
+
+public class MovieFavoritePresenter implements MovieFavouriteContract.Presenter {
+    @Override
+    public void loadFavoriteMovieFromRepository() {
+
+    }
+}
