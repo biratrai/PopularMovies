462e3bb33984b3f43ccdca6febe61855c3cccba3
Birat Rai <biratrai@hotmail.com>
2018-02-18 18:12:52 -0500

    getting the list of favorite movie from db

--- app/src/main/java/com/gooner10/popularmoviesapp/Activity/moviefavourite/MovieFavoritePresenter.java
+++ app/src/main/java/com/gooner10/popularmoviesapp/Activity/moviefavourite/MovieFavoritePresenter.java
@@ -1,12 +1,26 @@
 package com.gooner10.popularmoviesapp.Activity.moviefavourite;
 
+import android.content.Context;
+
+import com.gooner10.popularmoviesapp.Activity.data.MovieRepositoryImpl;
+
 /**
  * Presenter for the FavoriteFragment
  */
 
 public class MovieFavoritePresenter implements MovieFavouriteContract.Presenter {
+    private final MovieFavouriteContract.View view;
+    private MovieRepositoryImpl movieRepository;
+    private Context context;
+
+    public MovieFavoritePresenter(Context context, MovieFavouriteContract.View view) {
+        this.context = context;
+        this.view = view;
+    }
+
     @Override
     public void loadFavoriteMovieFromRepository() {
-
+        movieRepository = MovieRepositoryImpl.getMovieDatabaseInstance(context);
+        view.displayFavoriteMovie(movieRepository.getMovie());
     }
 }
