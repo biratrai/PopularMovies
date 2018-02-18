587a57ac23e9248ba202e54bc94bc21a380d6825
Birat Rai <biratrai@hotmail.com>
2018-02-18 18:03:35 -0500

    adding a adapter for the favorite movie fragment

---
+++ app/src/main/java/com/gooner10/popularmoviesapp/Activity/moviefavourite/FavoriteMovieFragmentAdapter.java
@@ -0,0 +1,58 @@
+package com.gooner10.popularmoviesapp.Activity.moviefavourite;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.gooner10.popularmoviesapp.Activity.data.MovieData;
+import com.gooner10.popularmoviesapp.R;
+
+import java.util.List;
+
+/**
+ * Adapter for Favorite Movie Fragment
+ */
+
+public class FavoriteMovieFragmentAdapter extends RecyclerView.Adapter<FavoriteMovieFragmentAdapter.MovieFavoriteViewHolder> {
+    private LayoutInflater layoutInflater;
+    private Context context;
+    private List<MovieData> movieData;
+
+    public FavoriteMovieFragmentAdapter(Context context, List<MovieData> movieData) {
+        this.context = context;
+        this.movieData = movieData;
+    }
+
+    @Override
+    public MovieFavoriteViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = layoutInflater.inflate(R.layout.favorite_movie_row, parent, false);
+        return new MovieFavoriteViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MovieFavoriteViewHolder holder, int position) {
+        holder.favoriteMovieText.setText(movieData.get(position).getMovieTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public void setData(List<MovieData> movieDataList) {
+        this.movieData = movieDataList;
+        notifyDataSetChanged();
+    }
+
+    public class MovieFavoriteViewHolder extends RecyclerView.ViewHolder {
+        TextView favoriteMovieText;
+
+        public MovieFavoriteViewHolder(View itemView) {
+            super(itemView);
+            favoriteMovieText = itemView.findViewById(R.id.favorite_movie_text);
+        }
+    }
+}
