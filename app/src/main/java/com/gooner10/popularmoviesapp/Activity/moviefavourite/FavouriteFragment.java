56d5cc05cec46aa808e978ece21f1fdf0ecb7625
Birat Rai <biratrai@hotmail.com>
2018-02-22 22:42:09 -0500

    adding fragment lifecycle for debug

--- app/src/main/java/com/gooner10/popularmoviesapp/Activity/moviefavourite/FavouriteFragment.java
+++ app/src/main/java/com/gooner10/popularmoviesapp/Activity/moviefavourite/FavouriteFragment.java
@@ -3,9 +3,12 @@ package com.gooner10.popularmoviesapp.Activity.moviefavourite;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,7 +27,7 @@ import hugo.weaving.DebugLog;
  * A simple {@link Fragment} subclass.
  */
 public class FavouriteFragment extends Fragment implements MovieFavouriteContract.View {
-
+    public static final String TAG = FavouriteFragment.class.getSimpleName();
     private FavoriteMovieFragmentAdapter favoriteMovieFragmentAdapter;
     MovieFavouriteContract.Presenter presenter;
 
@@ -43,13 +46,6 @@ public class FavouriteFragment extends Fragment implements MovieFavouriteContrac
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_favourite, container, false);
         ButterKnife.bind(this, view);
-        favoriteRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
-        favoriteMovieFragmentAdapter = new FavoriteMovieFragmentAdapter(getActivity(), new ArrayList<MovieData>());
-        favoriteRecyclerView.setAdapter(favoriteMovieFragmentAdapter);
-
-        presenter = new MovieFavoritePresenter(getActivity(), this);
-        setData();
-
         return view;
     }
 
@@ -61,8 +57,67 @@ public class FavouriteFragment extends Fragment implements MovieFavouriteContrac
 
     @Override
     @DebugLog
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    @DebugLog
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        favoriteRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+        favoriteMovieFragmentAdapter = new FavoriteMovieFragmentAdapter(getActivity(), new ArrayList<MovieData>());
+        favoriteRecyclerView.setAdapter(favoriteMovieFragmentAdapter);
+    }
+
+    @Override
+    @DebugLog
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        presenter = new MovieFavoritePresenter(getActivity(), this);
+        setData();
+    }
+
+    @Override
+    @DebugLog
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    @DebugLog
     public void onResume() {
         super.onResume();
+    }
+
+    @Override
+    @DebugLog
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    @DebugLog
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    @DebugLog
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+
+    @Override
+    @DebugLog
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    @DebugLog
+    public void onDetach() {
+        super.onDetach();
     }
 
     @Override
@@ -72,6 +127,7 @@ public class FavouriteFragment extends Fragment implements MovieFavouriteContrac
     }
 
     public void setData() {
+        Log.d(TAG, "setData: " + presenter);
         presenter.loadFavoriteMovieFromRepository();
     }
 
