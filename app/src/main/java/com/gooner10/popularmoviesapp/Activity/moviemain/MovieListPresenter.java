3afd7640fb396bfb4276b10619b923b418f8a32c
Birat Rai <biratrai@hotmail.com>
2018-02-23 23:23:12 -0500

    moving logic to viewmodel

--- app/src/main/java/com/gooner10/popularmoviesapp/Activity/moviemain/MovieListPresenter.java
+++ app/src/main/java/com/gooner10/popularmoviesapp/Activity/moviemain/MovieListPresenter.java
@@ -1,28 +1,22 @@
 package com.gooner10.popularmoviesapp.Activity.moviemain;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.Nullable;
 import android.util.Log;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.gooner10.popularmoviesapp.Activity.data.MovieData;
-import com.gooner10.popularmoviesapp.Activity.network.VolleySingleton;
+import com.gooner10.popularmoviesapp.Activity.data.MovieViewModel;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import java.util.List;
 
-import java.util.ArrayList;
+import hugo.weaving.DebugLog;
 
 /**
  * Presenter for the Movie
  */
 public class MovieListPresenter implements MovieContract.UserActionsListener {
     public static final String TAG = MovieListPresenter.class.getSimpleName();
-    private final String LOG_TAG = MovieListPresenter.class.getSimpleName();
-    private final ArrayList<MovieData> mMovieDataArrayList = new ArrayList<>();
     private final MovieContract.View movieView;
 
     public MovieListPresenter(MovieContract.View movieView) {
@@ -31,54 +25,17 @@ public class MovieListPresenter implements MovieContract.UserActionsListener {
 
     @Override
     public void loadData() {
-
-        JsonParser();
-        Log.i(LOG_TAG, "loadData");
-    }
-
-    private void JsonParser() {
-        final String url = "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=530c5cfd24953abae83df3e614c6d774";
-        Log.d("MovieActivity", "JsonParser");
-        RequestQueue requestQueue = VolleySingleton.getInstance().getRequestQueue();
-        JsonObjectRequest jsObjRequest = new JsonObjectRequest(Request.Method.GET, url, new Response.Listener<JSONObject>() {
+        MovieViewModel movieViewModel = ViewModelProviders.of((MovieFragment) movieView).get(MovieViewModel.class);
+        Log.i(TAG, "loadData " + movieView);
+        Log.i(TAG, "loadData getMovieDataList: "+ movieViewModel.getMovieDataList());
+        movieViewModel.getMovieDataList().observe((MovieFragment) movieView, new Observer<List<MovieData>>() {
             @Override
-            public void onResponse(JSONObject response) {
-                Log.d("MovieActivity", "" + response);
-                parseJSONresponse(response);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
+            @DebugLog
+            public void onChanged(@Nullable List<MovieData> movieDataList) {
+                movieView.displayMovieList(movieDataList);
             }
         });
-        requestQueue.add(jsObjRequest);
+
     }
 
-    private void parseJSONresponse(JSONObject response) {
-        try {
-            String jsonString = response.getString("results");
-
-            JSONArray jsonArray = new JSONArray(jsonString);
-            for (int i = 0; i < jsonArray.length(); i++) {
-                JSONObject jsonObject = (JSONObject) jsonArray.get(i);
-                String id = jsonObject.getString("id");
-                String title = jsonObject.getString("original_title");
-                String overview = jsonObject.getString("overview");
-                String release_date = jsonObject.getString("release_date");
-                String poster_path = jsonObject.getString("poster_path");
-                String backdrop_path = jsonObject.getString("backdrop_path");
-                String vote_average = jsonObject.getString("vote_average");
-                String vote_count = jsonObject.getString("vote_count");
-                String popularity = jsonObject.getString("popularity");
-                MovieData movieData
-                        = new MovieData(id, title, overview, poster_path, vote_average, vote_count,
-                        release_date, popularity, backdrop_path);
-                mMovieDataArrayList.add(movieData);
-            }
-
-        } catch (JSONException e) {
-            Log.e(TAG, "parseJSONresponse:", e);
-        }
-        movieView.displayMovieList(mMovieDataArrayList);
-    }
 }
