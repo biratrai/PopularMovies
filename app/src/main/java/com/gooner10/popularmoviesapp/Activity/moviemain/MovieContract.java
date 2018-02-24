d0b0697fb0d62506ef51e9991db4f786907b8ce8
Birat Rai <biratrai@hotmail.com>
2018-02-23 21:49:36 -0500

    using the interface contract

--- app/src/main/java/com/gooner10/popularmoviesapp/Activity/moviemain/MovieContract.java
+++ app/src/main/java/com/gooner10/popularmoviesapp/Activity/moviemain/MovieContract.java
@@ -3,13 +3,14 @@ package com.gooner10.popularmoviesapp.Activity.moviemain;
 import com.gooner10.popularmoviesapp.Activity.data.MovieData;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This specifies the contract between the Movie view and the Movie presenter.
  */
 public interface MovieContract {
     interface View {
-        void displayMovieList(ArrayList<MovieData> movieDataList);
+        void displayMovieList(List<MovieData> movieDataList);
     }
 
     interface UserActionsListener {
