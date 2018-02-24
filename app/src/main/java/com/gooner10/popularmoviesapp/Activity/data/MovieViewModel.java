99b12b38249d479a95b99cbd936c694c192db2ae
Birat Rai <biratrai@hotmail.com>
2018-02-23 21:39:32 -0500

    adding view model class

---
+++ app/src/main/java/com/gooner10/popularmoviesapp/Activity/data/MovieViewModel.java
@@ -0,0 +1,10 @@
+package com.gooner10.popularmoviesapp.Activity.data;
+
+import android.arch.lifecycle.ViewModel;
+
+/**
+ * ViewModel class that store the data fetched from back-end
+ */
+
+public class MovieViewModel extends ViewModel {
+}
