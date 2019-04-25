5f082aee25c5b40001cd3d4c951026d4b261de7a
Birat Rai <biratrai@hotmail.com>
2019-04-24 23:00:54 -0400

    moving the application class to new package

---
+++ app/src/main/java/com/gooner10/popularmoviesapp/MyApplication.java
@@ -0,0 +1,28 @@
+package com.gooner10.popularmoviesapp;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.facebook.stetho.Stetho;
+
+/**
+ * Application class for the app
+ */
+public class MyApplication extends Application {
+    private static MyApplication sInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sInstance = this;
+        Stetho.initializeWithDefaults(this);
+    }
+
+    public static MyApplication getInstance() {
+        return sInstance;
+    }
+
+    public static Context getAppContext() {
+        return sInstance.getApplicationContext();
+    }
+}
