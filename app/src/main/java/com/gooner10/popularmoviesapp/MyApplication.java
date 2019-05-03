f476a7d94c9183aaf36ab27c2c7396ec225ecc22
Birat Rai <biratrai@hotmail.com>
2019-05-02 22:42:23 -0400

    removing unnecessary static instance

--- app/src/main/java/com/gooner10/popularmoviesapp/MyApplication.java
+++ app/src/main/java/com/gooner10/popularmoviesapp/MyApplication.java
@@ -1,7 +1,6 @@
 package com.gooner10.popularmoviesapp;
 
 import android.app.Application;
-import android.content.Context;
 
 import com.facebook.stetho.Stetho;
 
@@ -9,20 +8,11 @@ import com.facebook.stetho.Stetho;
  * Application class for the app
  */
 public class MyApplication extends Application {
-    private static MyApplication sInstance;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        sInstance = this;
         Stetho.initializeWithDefaults(this);
     }
 
-    public static MyApplication getInstance() {
-        return sInstance;
-    }
-
-    public static Context getAppContext() {
-        return sInstance.getApplicationContext();
-    }
 }
