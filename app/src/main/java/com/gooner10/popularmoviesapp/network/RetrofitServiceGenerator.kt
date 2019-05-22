30bb88abf134d6188a0b9e7f7a668e9f41c45b29
Birat Rai <biratrai@hotmail.com>
2019-05-22 08:45:11 -0400

    converting RetrofitServiceGenerator to kotlin

---
+++ app/src/main/java/com/gooner10/popularmoviesapp/network/RetrofitServiceGenerator.kt
@@ -0,0 +1,38 @@
+package com.gooner10.popularmoviesapp.network
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+/**
+ * Class to generate instance of Retrofit Service
+ */
+class RetrofitServiceGenerator private constructor() {
+    init {
+        throw AssertionError("Cannot instantiate private constructor")
+    }
+
+    companion object {
+
+        private var builder: Retrofit.Builder? = null
+        private const val BASE_URL = "http://api.themoviedb.org/3/discover/"
+        private val httpClient = OkHttpClient()
+
+        private fun getBuilder(): Retrofit.Builder {
+            if (builder == null) {
+                builder = retrofit2.Retrofit.Builder()
+                        .baseUrl(BASE_URL)
+                        .addConverterFactory(GsonConverterFactory.create())
+            }
+            return builder!!
+        }
+
+        fun <S> createService(serviceClass: Class<S>): S {
+            val retrofit = getBuilder()
+                    .client(httpClient)
+                    .build()
+            return retrofit.create(serviceClass)
+        }
+    }
+
+}
