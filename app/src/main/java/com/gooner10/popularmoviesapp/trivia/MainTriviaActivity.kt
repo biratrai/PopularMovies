a3566194947e68a61582b76f96dd2230d84ed8fa
Birat Rai <biratrai@hotmail.com>
2019-05-29 08:51:01 -0400

    adding MainTriviaActivity

---
+++ app/src/main/java/com/gooner10/popularmoviesapp/trivia/MainTriviaActivity.kt
@@ -0,0 +1,12 @@
+package com.gooner10.popularmoviesapp.trivia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainTriviaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_trivia)
+    }
+}
