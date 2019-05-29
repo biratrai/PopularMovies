38cc64d5c38407637ec1740475f589da26da53de
Birat Rai <biratrai@hotmail.com>
2019-05-29 08:52:02 -0400

    setting click listener for button

--- app/src/main/java/com/gooner10/popularmoviesapp/trivia/MovieTriviaFragment.kt
+++ app/src/main/java/com/gooner10/popularmoviesapp/trivia/MovieTriviaFragment.kt
@@ -1,12 +1,14 @@
 package com.gooner10.popularmoviesapp.trivia
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.gooner10.popularmoviesapp.R
+import kotlinx.android.synthetic.main.fragment_movie_trivia.view.*
 
 
 /**
@@ -18,7 +20,10 @@ class MovieTriviaFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movie_trivia, container, false)
+        val view = inflater.inflate(R.layout.fragment_movie_trivia, container, false)
+        val button = view.triviaButton
+        button.setOnClickListener{ context?.startActivity(Intent(context, MainTriviaActivity::class.java)) }
+        return view
     }
 
 
