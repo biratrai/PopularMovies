0fa27b8e914c8c0cb3f9aba2a80dadbc33c9334a
Birat Rai <biratrai@hotmail.com>
2019-06-04 09:21:18 -0400

    adding navController to navigate to step_one_dest

--- app/src/main/java/com/gooner10/popularmoviesapp/trivia/HomeFragment.kt
+++ app/src/main/java/com/gooner10/popularmoviesapp/trivia/HomeFragment.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.gooner10.popularmoviesapp.R
 
 /**
@@ -18,6 +20,14 @@ class HomeFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val button = view.findViewById<Button>(R.id.navigate_destination_button)
+        button?.setOnClickListener {
+            findNavController().navigate(R.id.flow_step_one_dest, null)
+        }
     }
 
 
