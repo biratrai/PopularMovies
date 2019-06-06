cec6a7fbd6bbe5d955297d8549ed45c25a994648
Birat Rai <biratrai@hotmail.com>
2019-06-06 08:54:26 -0400

    adding animation slide for the start trivia button click

--- app/src/main/java/com/gooner10/popularmoviesapp/trivia/HomeFragment.kt
+++ app/src/main/java/com/gooner10/popularmoviesapp/trivia/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
 import com.gooner10.popularmoviesapp.R
 
 /**
@@ -25,10 +26,19 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val button = view.findViewById<Button>(R.id.navigate_destination_button)
-        button?.setOnClickListener {
-            findNavController().navigate(R.id.flow_step_one_dest, null)
+
+        val options = navOptions {
+            anim {
+                enter = R.anim.slide_in_right
+                exit = R.anim.slide_out_left
+                popEnter = R.anim.slide_in_left
+                popExit = R.anim.slide_out_right
+            }
+        }
+
+        button.setOnClickListener {
+            findNavController().navigate(R.id.flow_step_one_dest, null, options)
         }
     }
-
 
 }
