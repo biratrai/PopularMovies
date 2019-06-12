ec63e38e7309b18458dafb983a5e42e9a05bb4fd
Birat Rai <biratrai@hotmail.com>
2019-06-12 08:54:42 -0400

    using the safeArgs to get the flowStepNumber and inflate the view accordingly

--- app/src/main/java/com/gooner10/popularmoviesapp/trivia/FlowStepFragment.kt
+++ app/src/main/java/com/gooner10/popularmoviesapp/trivia/FlowStepFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.navArgs
 import com.gooner10.popularmoviesapp.R
 
 /**
@@ -16,8 +18,24 @@ class FlowStepFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+        val safeArgs: FlowStepFragmentArgs by navArgs()
+        val flowStepNumber = safeArgs.flowStepNumber
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_flow_step_one, container, false)
+        return when (flowStepNumber) {
+            2 ->
+                inflater.inflate(R.layout.fragment_flow_step_two, container, false)
+            else ->
+                inflater.inflate(R.layout.fragment_flow_step_one, container, false)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<View>(R.id.next_button).setOnClickListener(
+                Navigation.createNavigateOnClickListener(R.id.next_action)
+        )
     }
 
 
