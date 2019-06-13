366f9fa961eb0963b49b44b3f0ed61a59543ceaf
Birat Rai <biratrai@hotmail.com>
2019-06-13 08:53:37 -0400

    adding toolbar and appBarConfiguration for navController

--- app/src/main/java/com/gooner10/popularmoviesapp/trivia/MainTriviaActivity.kt
+++ app/src/main/java/com/gooner10/popularmoviesapp/trivia/MainTriviaActivity.kt
@@ -2,12 +2,27 @@ package com.gooner10.popularmoviesapp.trivia
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import com.gooner10.popularmoviesapp.R
 
 class MainTriviaActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_trivia)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val host: NavHostFragment = supportFragmentManager
+                .findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment? ?: return
+
+        val navController = host.navController
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
     }
+
 }
